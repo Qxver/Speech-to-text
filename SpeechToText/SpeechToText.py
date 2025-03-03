3720d62a508e3f71c70d6e6ef639befb0aee6e09
Wojciech Paradziński <84313530+Qxver@users.noreply.github.com>
2025-03-03 19:18:54 +0100

    Add files via upload

--- SpeechToText/SpeechToText.py
+++ SpeechToText/SpeechToText.py
@@ -2,27 +2,45 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 
 
-def speech_to_text() -> None:
-    model = Model(r"C:\Users\User\PycharmProjects\SpeechToText\vosk-model-small-en-us-0.15")  # English
-    #model = Model(r"C:\Users\User\PycharmProjects\SpeechToText\vosk-model-small-pl-0.22")  # Polish
-    recognizer = KaldiRecognizer(model, 16000)
+class SpeechToText:
+    def __init__(self, model) -> None:
+        self.model = Model(model)
+        self.recognizer = KaldiRecognizer(self.model, 16000)
+        self.microphone = pyaudio.PyAudio()
+        self.stream = self.microphone.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=16000,
+            input=True,
+            frames_per_buffer=8192
+        )
+        self.stream.start_stream()
 
-    microphone = pyaudio.PyAudio()
-    stream = microphone.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-    stream.start_stream()
+    def recognize(self) -> None:
+        while True:
+            data = self.stream.read(4096)
 
-    while True:
-        data = stream.read(4096)
+            if self.recognizer.AcceptWaveform(data):
+                result = self.recognizer.Result()
+                text = result[14:-3]
+                print(text)
 
-        if recognizer.AcceptWaveform(data):
-            text = recognizer.Result()
-            text = text[14:-3]
-            print(text)
+                if text in ("stop", "quit", "end", "exit"):
+                    print("Stopping the program...")
+                    break
 
-            if text == "stop" or text == "quit":
-                print("Stopping the program...")
-                break
+    def close(self) -> None:
+        self.stream.stop_stream()
+        self.stream.close()
+        self.microphone.terminate()
 
 
-speech_to_text()
-        
+if __name__ == "__main__":
+    model_path = r"C:\Users\User\PycharmProjects\SpeechToText\vosk-model-small-en-us-0.15"  # English
+    # model_path = r"C:\Users\User\PycharmProjects\SpeechToText\vosk-model-small-pl-0.22"  # Polish
+    stt = SpeechToText(model_path)
+
+    try:
+        stt.recognize()
+    finally:
+        stt.close()
